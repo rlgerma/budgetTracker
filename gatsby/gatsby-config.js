b95eac964ed633c9b8a4d7d6e088356321c07a3b
Richard Germaine <rlgerma92@outlook.com>
2020-02-12 11:46:41 -0700

    added gatsby framework

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  siteMetadata: {
+    title: `Budget Tracker`,
+    description: `A simple one page Budget Tracker`,
+    author: `Richard Germaine`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Poppins`,
+            variants: [`300`, `400`, `600`, `700`],
+          },
+        ],
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `product`,
+        path: `${__dirname}/src/images/product`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-source-mongodb`,
+      options: {
+        dbName: `budget`,
+        collection: `myMoney`,
+        // highlight-start
+        map: {
+          documents: { body: `text/markdown` },
+        },
+      },
+      // this (optional) plugin enables Progressive Web App + Offline functionality
+      // To learn more, visit: https://gatsby.dev/offline
+      // `gatsby-plugin-offline`,
+    },
+  ],
+}
